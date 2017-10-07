c240e347352ad3216ff52b8295259d1b89a8d258
schreck <schreckengost@gmail.com>
2017-10-07 10:23:25 -0400

    got the getpixels to work, needed rawer AndroidJNI calles to send the thing native java int[] pointer that would be mutated by the Bitmap.getPixels() call

--- Assets/radar_plain.cs
+++ Assets/radar_plain.cs
@@ -44,14 +44,32 @@ public class radar_plain : MonoBehaviour {
         con.add("height " + h);
         int w = bmo.Call<int>("getWidth", new object[] { });
         con.add("width " + w);
-        int[] pixs;
+        System.IntPtr pixs = AndroidJNI.NewIntArray(h * w);
         var texture = new Texture2D(w, h, TextureFormat.ARGB32, false);
+        //bmo.Call("getPixels", new object[] { pixs, 0, w, 0, 0, w, h });
+        jvalue[] gpargs;
+        gpargs = new jvalue[7];
+        gpargs[0].l = pixs;
+        gpargs[1].i = 0;
+        gpargs[2].i = w;
+        gpargs[3].i = 0;
+        gpargs[4].i = 0;
+        gpargs[5].i = w;
+        gpargs[6].i = h;
+        con.add("gpargs");
+        AndroidJNI.CallVoidMethod(bmo.GetRawObject(), AndroidJNI.GetMethodID(bm.GetRawClass(), "getPixels","([IIIIIII)V"), gpargs);
+        con.add("getpixs");
+        int[] apixs;
+        apixs = new int[w * h];
+        apixs = AndroidJNI.FromIntArray(pixs);
+        con.add("apixs");
         for (int i = 0; i < h; i++)
         {
             for(int j = 0; j < w; j++)
             {
                 //con.add("(" + j + "," + i + ")");
-                int pixel = bmo.Call<int>("getPixel", new object[] { j, i });
+                //int pixel = bmo.Call<int>("getPixel", new object[] { j, i });
+                int pixel = apixs[j + w * i];
                 //con.add("pixel " + pixel);
                 Color32 pc = ConvertAndroidColor(pixel);
                 //con.add("color");
@@ -59,12 +77,14 @@ public class radar_plain : MonoBehaviour {
                 //con.add("Set pizxel");
             }
         }
-        con.add("pix");
+        con.add("pix2" + apixs.GetHashCode());
         texture.Apply();
         con.add("txt");
         plane = transform.GetChild(0);
         plane.GetComponent<Renderer>().material.mainTexture = texture;
         con.add("mesh");
+        
+
         yield return "good";
     }
 
